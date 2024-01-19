f1ae136e0fbbc8484a0eebb856043ae7b0f24bde
haazzero <ha020@naver.com>
2024-01-19 14:27:53 +0900

    커밋테스트

--- pythonWord.py
+++ pythonWord.py
@@ -1,17 +1,23 @@
+# Word 라이브러리
 import docx
 from docx import Document
+# Excel 라이브러리
 import openpyxl
 from openpyxl import Workbook
+# 파일 로컬에서 열기
 import win32com.client
+# 웹 크롤링
 import requests
 from bs4 import BeautifulSoup
 
-# Excel 어플리케이션 열기
+# excel 어플리케이션 열기
 excel = win32com.client.gencache.EnsureDispatch("Excel.application")
+# 자동으로 실행되는 것을 확인하고 싶을 때 
 excel.Visible = True
 
 # 워드 파일 경로 입력받기
-word_file_path = input('워드 파일 경로를 입력해주세요.').strip('"')
+# word_file_path = input('워드 파일 경로를 입력해주세요.').strip('"')
+word_file_path = "C:\\Users\\hy\\OneDrive\\eng\\Short Essay_hy.docx"
 
 # 워드 파일 열기
 doc = Document(word_file_path)
@@ -45,33 +51,43 @@ for word in highlighted_text:
         highlighted_text_korean.append(korean)
 
 # 결과 출력
-print('*************** 형광펜 단어에 대한 정보를 모았어요! ***************')
-print(f'*************** 모르는 단어 수: {len(highlighted_text)}')
+print(' *************** 형광펜 단어에 대한 정보를 모았어요 ! ')
+print(f'*************** 모르는 단어 수 : {len(highlighted_text)}')
 for i in range(len(highlighted_text)):
     print(f"단어: {highlighted_text[i]}")
     print(f"문장: {highlighted_text_sentence[i]}")
     print(f"뜻: {highlighted_text_korean[i]}")
 
-# Excel 파일 생성 및 데이터 입력
+##################### Excel
+
+# Workbook 객체 생성
 wb = Workbook()
+# 활성화된 워크시트 선택 후 ws 변수에 할당
 ws = wb.active
+# 시트 제목을 Highlighted Word로 설정
 ws.title = "Highlighted Words" 
 
-header = ['번호', '단어', '뜻', '문장']
+# 첫 행에 인덱스
+header = ['번호','단어','뜻','문장']
 ws.append(header)
 
+# highlighted_text에 담긴 단어 및 해당 단어가 포함된 문장을 for문으로 'A'열의 각 셀에 순서대로 접근하여 입력
 for i in range(len(highlighted_text)):
     word = highlighted_text[i]
     sentence = highlighted_text_sentence[i]
     mean = highlighted_text_korean[i]
-    ws.append([i+1, word, mean, sentence])
+    ws.append([i+1, word, mean, sentence])  # 엑셀 행 추가
 
+# 엑셀 파일 경로 및 제목 설정
 excel_file_path = word_file_path.replace('.docx', '_voca.xlsx')
+# 워크북 엑셀 파일 저장 (제목)
 wb.save(excel_file_path)
+
+####################################
 
 # 저장된 엑셀 파일 열기
 excel.Workbooks.Open(excel_file_path)
-print(f'저장된 엑셀 파일 경로: {excel_file_path}')
+print(f'저장 된 엑셀 파일 경로 : {excel_file_path}')
 
 # 워크북 닫기
 wb.close()
